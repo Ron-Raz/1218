bd213c6881349c6a4d556a8e7f224e02b60fc8f3
Ron Raz <ron.raz@kaltura.com>
2018-12-31 16:31:04 -0500

    Update kms.js

--- kms.js
+++ kms.js
@@ -1,6 +1,6 @@
 $(function() {
   $('link[rel="icon"]').attr('href', 'https://i.ibb.co/5GgsZPF/favicon-bnsf.png'); 
-  var $up=$("#uploadContainer > h1");
-  console.log("up len",$up.length)
+  var $up=$("#uploadContainer>h1");
+  console.log("up len",$up.length,$up);
 });
 
