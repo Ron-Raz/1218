2a22be669144b7519260f923f2644776aa3442ba
Ron Raz <ron.raz@kaltura.com>
2018-12-31 17:47:17 -0500

    Update kms.js

--- kms.js
+++ kms.js
@@ -1,13 +1,13 @@
 $(function() {
-  console.log("ver6");
+  console.log("ver7");
   // update favicon
   $('link[rel="icon"]').attr('href', 'https://i.ibb.co/5GgsZPF/favicon-bnsf.png'); 
   // check if creating live entry
   var $liveEntry=$("#Entry-submit");
   if( $liveEntry.length === 0 ) {
+    console.log("hide sip field");
     // this is not a live entry. disable the sip field
-    var $sipField= $("#customdata-SIP");
-    $sipField.hide();
+    $("#customdata-SIP").hide();
   }
 });
 
