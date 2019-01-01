b24ec43ffc8c991b7c5c2356bee7e6c479e3c276
Ron Raz <ron.raz@kaltura.com>
2019-01-01 01:54:19 -0500

    Update kms.js

--- kms.js
+++ kms.js
@@ -4,7 +4,7 @@ $(function() {
 	$('link[rel="icon"]').attr('href', 'https://i.ibb.co/5GgsZPF/favicon-bnsf.png'); 
 	// show SIP fields when creating entry
 	$( "#Entry-name" ).on( "focus", function() {
-		console.log( $( this ).text() );
+		console.log( "entry name focus. len=",$("#Entry-submit").length );
 	});
 	/*
 	var $createEntry= $("#Entry-submit");
