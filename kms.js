75c06795ec5e48aeb13aa24d859ffafe67ce96ce
Ron Raz <ron.raz@kaltura.com>
2019-01-01 02:02:15 -0500

    Update kms.js

--- kms.js
+++ kms.js
@@ -1,19 +1,17 @@
 $(function() {
-	console.log("ver12");
+	console.log("ver13");
 	// update favicon
 	$('link[rel="icon"]').attr('href', 'https://i.ibb.co/5GgsZPF/favicon-bnsf.png'); 
 	// show SIP fields when creating entry
 	$( "#Entry-name" ).on( "focus", function() {
 		console.log( "entry name focus. len=",$("#Entry-submit").length );
+		var $createEntry= $("#Entry-submit");
+		if( $("#Entry-submit") > 0 ) {
+			$("#customdata-SIP, #customdata-SIP+P, #customdata-SIP-label").show();
+			console.log("showing fields");
+		} else {
+			console.log("keeping fields hidden");
+		}
 	});
-	/*
-	var $createEntry= $("#Entry-submit");
-	if( $createEntry > 0 ) {
-		$("#customdata-SIP, #customdata-SIP+P, #customdata-SIP-label").show();
-		console.log("showing fields");
-	} else {
-		console.log("keeping fields hidden");
-	}
-	*/
 });
 
