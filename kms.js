bce911e546ecc4a62d7b123e20529cdbb9bdaaf4
Ron Raz <ron.raz@kaltura.com>
2019-01-01 09:55:01 -0500

    Update kms.js

--- kms.js
+++ kms.js
@@ -11,7 +11,7 @@ function addCSSRule(sheet, selector, rules, index) {
 
 $(function() {
 	var wlp= window.location.pathname;
-	console.log("ver19", wlp);
+	console.log("ver20", wlp);
 	/*
 	pathname		action
 	========		======
@@ -21,12 +21,11 @@ $(function() {
 	// update favicon
 	$('link[rel="icon"]').attr('href', 'https://i.ibb.co/5GgsZPF/favicon-bnsf.png'); 
 	
-	// show SIP fields when creating, editing, viewing live entry
+	// show SIP fields ony when creating or editing a live entry
 	var res= wlp.split('/');
 	if( wlp === '/kwebcast/entry/add' || ($("#KwebcastAdvancedOptions-tab").length > 0) ) {
 		console.log("don't hide sip field");
 	} else {
-		
 		addCSSRule(document.styleSheets[0], "#customdata-SIP, #customdata-SIP+P, #customdata-SIP-label, #entry-metadata", "display: none");
 		console.log("rule added to hide sip field");
 	}
